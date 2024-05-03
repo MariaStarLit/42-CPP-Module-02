968066369cdfe38bb69879bae7d7e526d36c9032
Maria De Avelar Galvao Santos Lima <mde-avel@c1r19s10.42lisboa.com>
2024-05-03 19:45:06 +0100

    fixed

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,13 +6,22 @@
 /*   By: mde-avel <mde-avel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 19:21:50 by mde-avel          #+#    #+#             */
-/*   Updated: 2024/05/03 19:37:16 by mde-avel         ###   ########.fr       */
+/*   Updated: 2024/05/03 19:44:37 by mde-avel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+
+// Colors
+# define RESET "\033[0m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
 
 class Fixed
 {
